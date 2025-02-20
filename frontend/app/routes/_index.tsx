3aa0dce21dd6d34faaaa671b05b13e0bf9463e78
nova-27 <33594303+nova-27@users.noreply.github.com>
2025-02-20 18:03:15 +0900

    fix: 色をcss変数定義する

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -10,19 +10,6 @@ export const meta: MetaFunction = () => {
 
 type RecordingState = "idle" | "processing" | "recording";
 
-/*
-NOTE: 色を一か所で管理するために定義
-tailwindのclassNameを定義するとborderやtext、bgなど量が多いためカラーコードで管理してます
-適用はやむを得なくstyleで指定しています
-気に入らなかったらclassNameで置き換えてください
-*/
-const foregroundColors = {
-  default: "#f7bb43",
-  error: "#ea5477",
-};
-
-const backgroundColor = "#13141b";
-
 const oracleText = {
   idle: "Read this sentence for me and I'll guess your accent.",
   recording: "Read this sentence for me and I'll guess your accent.",
@@ -31,7 +18,7 @@ const oracleText = {
 } satisfies Record<"error" | RecordingState, string>;
 
 const buttonIcon = {
-  idle: <FaMicrophone className="w-[46px] h-[46px]" style={{ color: backgroundColor }} />,
+  idle: <FaMicrophone className="w-[46px] h-[46px] text-bg-primary" />,
   recording: <FaRegSquare className="w-[40px] h-[40px] text-white" />,
   processing: <AiOutlineLoading className="animate-spin w-[46px] h-[46px] text-white" />,
 } satisfies Record<RecordingState, JSX.Element>;
@@ -88,8 +75,6 @@ export default function Index() {
     setRecordingState("processing");
   };
 
-  const foregroundColor = error ? foregroundColors.error : foregroundColors.default;
-
   return (
     <div className="flex flex-1 flex-col justify-between items-center self-stretch lg:min-h-[456px] min-h-[522px]">
       <div className="gap-[6px] flex items-start self-start min-h-[110px]">
@@ -97,18 +82,17 @@ export default function Index() {
           <div className="w-full h-full bg-red-500 rounded-full" />
         </div>
         <div
-          className="flex flex-col justify-center items-start gap-[2px] py-3 px-4 border rounded-[20px] rounded-tl-none"
-          style={{ borderColor: foregroundColor }}
+          className={`flex flex-col justify-center items-start gap-[2px] py-3 px-4 border rounded-[20px] rounded-tl-none ${error ? "border-error" : "border-recording"}`}
         >
-          <p className="font-bold text-sm leading-[130%]" style={{ color: foregroundColor }}>
-            The Oracle
-          </p>
+          <p className={`font-bold text-sm leading-[130%] ${error ? "text-error" : "text-recording"}`}>The Oracle</p>
           <p className="text-white font-medium text-base leading-[130%]">
             {error ? oracleText.error : oracleText[recordingState]}
           </p>
         </div>
       </div>
-      <div className="lg:text-4xl text-xl tracking-[-0.6px] lg:text-center" style={{ color: foregroundColor }}>
+      <div
+        className={`lg:text-4xl text-xl tracking-[-0.6px] lg:text-center ${error ? "text-error" : "text-recording"}`}
+      >
         I love exploring local markets and trying out different foods. Whether it&apos;s fresh produce, unique spices,
         or handmade treats, it&apos;s exciting to discover new flavors. I always leave inspired to cook something
         creative at home.
@@ -116,22 +100,15 @@ export default function Index() {
       <div className="flex w-full relative flex-col justify-end items-center gap-[12px]">
         <div className="flex justify-center items-center relative overflow-visible h-[90px]">
           <button
-            className="z-20 cursor-pointer flex transition-all duration-200 justify-center items-center rounded-full w-[90px] h-[90px] border"
+            className={`z-20 cursor-pointer flex transition-all duration-200 justify-center items-center rounded-full w-[90px] h-[90px] border ${recordingState === "idle" ? "border-white bg-white" : "border-recording bg-bg-primary"}`}
             disabled={recordingState === "processing"}
             onClick={recordingState === "idle" ? startRecording : stopRecording}
-            style={{
-              backgroundColor: recordingState === "idle" ? "white" : backgroundColor,
-              borderColor: recordingState === "idle" ? "white" : foregroundColor,
-            }}
             type="button"
           >
             {buttonIcon[recordingState]}
           </button>
           <div
-            className="w-[504px] h-[274px] transition-all will-change-transform absolute left-1/2 transform -translate-x-1/2 blur-[77px]"
-            style={{
-              backgroundImage: `radial-gradient(50% 50% at 50% 50%, color-mix(in srgb, ${foregroundColor} 60%, transparent) 20%, color-mix(in srgb, ${backgroundColor} 60%, transparent) 100%)`,
-            }}
+            className={`w-[504px] h-[274px] transition-all will-change-transform absolute left-1/2 transform -translate-x-1/2 blur-[77px] bg-radial-[50%_50%_at_50%_50%] ${error ? "from-error-60" : "from-recording-60"} from-20% to-bg-primary-60`}
           />
         </div>
         <div className="flex flex-col justify-center items-center z-10">
@@ -145,16 +122,13 @@ export default function Index() {
           >
             <title>Arrow</title>
             <path
+              className="fill-bg-primary"
               d="M20.5 0L13.5465 6.7712C11.8639 8.4096 9.13605 8.4096 7.45353 6.7712L0.5 0H20.5Z"
-              fill={backgroundColor}
             />
           </svg>
-          <div
-            className="flex max-w-[280px] py-[6px] px-[12px] justify-center items-center gap-[7.5px] rounded-lg"
-            style={{ backgroundColor: backgroundColor }}
-          >
-            {recordingState === "recording" && <div className="bg-[#1aa23a] w-2 h-2 rounded-full" />}
-            <p className="text-center text-sm leading-[120%] font-medium" style={{ color: foregroundColor }}>
+          <div className="flex max-w-[280px] py-[6px] px-[12px] justify-center items-center gap-[7.5px] rounded-lg bg-bg-primary">
+            {recordingState === "recording" && <div className="bg-listening w-2 h-2 rounded-full" />}
+            <p className={`text-center text-sm leading-[120%] font-medium ${error ? "text-error" : "text-recording"}`}>
               {error ?? buttonText[recordingState]}
             </p>
           </div>
